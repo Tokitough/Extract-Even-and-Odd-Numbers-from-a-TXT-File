086c64a3926291d6adc7d6c484d854c2bbf49ec1
Kaiser Sajo <jhonkaisersajo@gmail.com>
2023-04-28 22:50:35 +0800

    Extract even and odd numbers

--- Numbers.py
+++ Numbers.py
@@ -4,4 +4,9 @@ with open("numbers.txt" , "r") as my_file:
     with open("odd.txt" , "w") as odd_file , open("even.txt" , "w") as even_file:
         for line in my_file:
             integer = int(line.strip())
-            
+             # Extract even numbers
+            if integer % 2 == 0:
+                even_file.write(str(integer) + "\n")
+            # Extract odd numbers
+            else:
+                odd_file.write(str(integer) + "\n")
