4895c12dd64878d386f9c0a5462cf0fb98226a14
Kaiser Sajo <jhonkaisersajo@gmail.com>
2023-04-28 22:49:22 +0800

    Create text files for odd and even numbers

--- Numbers.py
+++ Numbers.py
@@ -1,3 +1,7 @@
 # Read a text file
 with open("numbers.txt" , "r") as my_file:
-    
+    # Create two other text files for odd and even numbers
+    with open("odd.txt" , "w") as odd_file , open("even.txt" , "w") as even_file:
+        for line in my_file:
+            integer = int(line.strip())
+            
